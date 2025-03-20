6a0bd43d864fff29659eef92a44236eb88fdbb3a
Reinaldo Alves de Oliveira <reinaldoo.iot@gmail.com>
2025-03-20 16:16:36 -0400

    Add class filme e serie
    criacao classes que estarao recebendo a heranca

---
+++ entreterimentoHeranca/src/Filme.java
@@ -0,0 +1,7 @@
+public class Filme extends Titulo{
+
+
+    public Filme(String titulo, int anoDeLancamento) {
+        super(titulo, anoDeLancamento);
+    }
+}
---
+++ entreterimentoHeranca/src/Serie.java
@@ -0,0 +1,5 @@
+public class Serie extends Titulo{
+    public Serie(String titulo, int anoDeLancamento) {
+        super(titulo, anoDeLancamento);
+    }
+}
