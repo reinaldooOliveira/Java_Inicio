3dda5c9b4116a319c24565af05c04c708bf21ded
Reinaldo Alves de Oliveira <reinaldoo.iot@gmail.com>
2025-03-20 19:30:18 -0400

    update finalizacao code
    conclusao atividade exemplo codigo Heranca

--- entreterimentoHeranca/src/Main.java
+++ entreterimentoHeranca/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Filme filme = new Filme(" The Matrix ", 1999);
+        System.out.println("Titulo:" + filme.getTitulo() + "| Ano Lançamento: " +
+                filme.getAnoDeLancamento());
+
+        Serie serie = new Serie(" The Big Bag: Theory ", 2007);
+        System.out.println("Titulo:" + serie.getTitulo() + "| Ano Lançamento: " +
+                serie.getAnoDeLancamento());
     }
 }
